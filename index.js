1a2180f73ba01caeb1fe7b54cf3a588a60fb913f
Akash Sinha <aksinhaemail.com>
2022-07-29 13:17:31 +0530

    testing commit

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require("express")
 const cors = require("cors")
+const camlist = require("./api/camlist")
 
 const app = express()
 app.use(cors())
 const PORT = 3000
 
-app.get("/camlist", (req, res) => {
-  res.send({
-    hello: "this is a message",
-  })
-})
+app.use("/api/camlist", camlist)
 app.listen(PORT, () => {
   console.log("server is listening on port 3000")
 })
